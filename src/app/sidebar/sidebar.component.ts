e7a4794724d1312a8e81f4c9ad1682c8b898f293
Eugene Kitsios <eugene.kitsios@gmail.com>
2018-10-02 16:39:36 +0200

    versiondinsdag3

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../service/auth.service';
+import { RegisterserviceService } from '../../service/registerservice.service';
+import { UserDTO } from '../../model/user-dto';
+
 
 @Component({
   selector: 'app-sidebar',
@@ -10,6 +13,14 @@ export class SidebarComponent implements OnInit {
 
   constructor(private service:AuthService) { }
 
+  public user = new UserDTO();
+  private userUrl = 'http://localhost:9090/user/'; 
+  username:String
+
+ public arrayFromStroage = JSON.parse(localStorage.getItem("user2"));
+ public arrayLength = this.arrayFromStroage.length;
+  
+
   ngOnInit() {
   }
 
@@ -17,4 +28,23 @@ export class SidebarComponent implements OnInit {
     this.service.logout();
 }
 
+  showInfo() {
+    alert("test"); 
+      this.service.getResource(this.userUrl)
+       .subscribe(
+                   data => {
+                    this.user=data,           
+                    
+                    this.username=this.user.principal.username                              
+                  }
+                   
+                  );
+
+                  alert("Username: " + this.user.principal.username)
+                  alert(this.arrayFromStroage)
+                
+  }
+  
 }
+
+
